9140e8a04b4fcf83ce4d0fff5ee864fdbedc5251
md rayhan <43013813+importrayhan@users.noreply.github.com>
2021-01-02 16:20:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ def intro_image():
   d = ImageDraw.Draw(im) 
   d.text((250, 40), "Identify Your", fill="OrangeRed", anchor="ms", font=font)
 
-  font = ImageFont.truetype("NebulousRegular-54aV.ttf", 500)
+  font = ImageFont.truetype("NebulousRegular-54aV.ttf", 600)
   im2 = Image.open('style.jpg')
   d = ImageDraw.Draw(im2) 
-  d.text((400, 400), "Learning", fill="LightYellow", anchor="ms", font=font)
+  d.text((900, 600), "Learning", fill="LightYellow", anchor="ms", font=font)
   return im,im2
 
 im , im2 = intro_image()
